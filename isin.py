b0a9ba6bbc0e765a0887fde277a556539352ff4c
SSoG Cleobulus <przemyslaw.jarog@gmail.com>
2019-05-06 23:06:10 +0200

    Added funct/incomplete

--- isin.py
+++ isin.py
@@ -11,4 +11,11 @@ def sortString(x):
     for el in x:
         ans += el
     return ans
+
+def bisectionRecur():
+    if len(x) % 2 == 0:
+        if x[len(x)/2] == base:
+            return base
+        elif x[len(x)/2] < base:
+        
 print(sortString('bdackjoe'))
