79e7170628fbb974f656fa7632bb18544a1c0b89
SSoG Cleobulus <przemyslaw.jarog@gmail.com>
2019-06-20 08:36:16 +0200

    Fixed and working

--- primality.py
+++ primality.py
@@ -3,11 +3,14 @@ def getNumber():
     return int(input('Give me a number: '))
 
 def checkNumber(number):
-    for i in range(1, number):
-        if number % i == 0 and number % number == 0:
-            return True
-        else:
-            return False
+    if number == 2 or number == 3:
+        return True
+    else:
+        for i in range(2, number-1):
+            if number % i != 0:
+                return True
+            else:
+                return False
 
 num = getNumber()
 result = checkNumber(num)
